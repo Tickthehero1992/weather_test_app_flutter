c75ceca1f6ff993690edee0f64defadabb6bcbf1
Tickthehero1992 <pereirasanchez@mail.ru>
2025-05-14 11:01:41 +0300

    add save remoteId to phone

--- lib/ble_worker/ble_class_works.dart
+++ lib/ble_worker/ble_class_works.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import "dart:io";
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -9,11 +9,35 @@ class BLEWorker extends GetxController{
 
   //Stream <List<ScanResult>> stream = Stream.empty();
   Stream<List<ScanResult>>  scanResult = Stream.empty();
+  bool connectedDevice = false;
+  final  String fileInfo = "/infoBle";
+  String deviceId = "";
+  BluetoothDevice? devicePair;
+  BLEWorker() {
+    if (File(fileInfo).existsSync()) {
+      deviceId = File(fileInfo).readAsStringSync();
+      devicePair = BluetoothDevice.fromId(deviceId);
+    }
+  }
+
+  Future<String> firstConnection() async{
+    if(devicePair != null)
+      {
+        await scanDevices();
+        if(await FlutterBluePlus.scanResults.contains(devicePair) == true)
+        {
+          await devicePair!.connect(autoConnect: true);
+          await devicePair!.connectionState.where((val) => val == BluetoothConnectionState.connected).first;
+          return devicePair!.advName.toString();
+        }
+      }
+    return "No saved Device";
+}
+
   Future scanDevices() async{
     bool connectGranted = await Permission.bluetoothConnect.request().isGranted;
     bool scanGranted = await Permission.bluetoothScan.request().isGranted;
     bool st = await Permission.bluetoothAdvertise.request().isGranted;
-    bool stateBle = false;
     if(connectGranted && scanGranted && st){
       var subscription = FlutterBluePlus.onScanResults.listen((results){
         if(results.isNotEmpty){
@@ -50,11 +74,18 @@ class BLEWorker extends GetxController{
    var subscribe =  device.connectionState.listen((isConnected){
      if(isConnected == BluetoothConnectionState.disconnected)
        {
+         print("${device.disconnectReason?.code} ${device.disconnectReason?.description}");
          device.connect(timeout:Duration(seconds: 15));
+       }
+     else
+       {
+         connectedDevice = true;
        }
     });
    device.cancelWhenDisconnected(subscribe, delayed: true, next:true);
-   await device.connect(timeout:Duration(seconds: 15));
+   await device.connect(autoConnect: true, timeout:Duration(seconds: 15));
+   await File(fileInfo).writeAsString(device.remoteId.str);
+
    subscribe.cancel();
   }
 }
