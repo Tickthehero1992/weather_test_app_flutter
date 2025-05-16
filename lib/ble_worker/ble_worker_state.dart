7ecba14643a1cefc454133856459e40370c1a05a
Tickthehero1992 <pereirasanchez@mail.ru>
2025-05-16 08:40:29 +0300

    error of init

--- lib/ble_worker/ble_worker_state.dart
+++ lib/ble_worker/ble_worker_state.dart
@@ -3,4 +3,20 @@ part of 'ble_worker_bloc.dart';
 @immutable
 sealed class BleWorkerState {}
 
-final class BleWorkerInitial extends BleWorkerState {}
+final class BleWorkerInitial extends BleWorkerState {
+
+}
+
+final class BleWorkerScan extends BleWorkerState{}
+
+final class BleWorkerScanSuccess extends BleWorkerState{}
+
+final class BleWorkerConnect extends BleWorkerState{}
+
+final class BleWorkerConnectSuccess extends BleWorkerState{}
+
+final class BleWorkerGetCharacteristics extends BleWorkerState{}
+
+final class BleWorkerGetCharacteristicsSuccess extends BleWorkerState{}
+
+
